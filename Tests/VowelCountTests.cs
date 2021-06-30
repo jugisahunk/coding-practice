2f5faa53a187ba19f7f878c3f5f57f2153b9ae02
Jason Knight <jugisahunk@gmail.com>
2021-06-30 13:30:39 -0500

    passed 'Abracadabra' test

--- Tests/VowelCountTests.cs
+++ Tests/VowelCountTests.cs
@@ -1,29 +1,15 @@
 ﻿using Xunit;
 using CodingPractice;
 
-namespace CodingPracticeTests
+namespace Tests
 {
     public class VowelCountTests
     {
 
         [Fact]
-        public void Given_word_Return_vowel_count()
+        public void Given_Abracadabra_Return_FiveCount()
         {
-            Assert.Equal(3, Kata.GetVowelCount("Hello World!"));
-        }
-
-
-        [Fact]
-        public void Given_long_word_Return_vowel_count()
-        {
-            Assert.Equal(5, Kata.GetVowelCount("abracadabra"));
-        }
-
-
-        [Fact]
-        public void Given_no_vowels_Return_count_zero()
-        {
-            Assert.Equal(0, Kata.GetVowelCount("qwrtyplkjhgsdf"));
+            Assert.Equal(5,Kata.GetVowelCount("Abracadabra"));
         }
 
     }
